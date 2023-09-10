28de838d51460024e0402a601dd14b8b554c43bd
Parames Pasitprachaja <parames.pasitprachaja@gmail.com>
2023-09-10 20:12:31 +0700

    feat(delete-document): create basic controller to delete document by id

--- utils-backend/controller/basic/index.ts
+++ utils-backend/controller/basic/index.ts
@@ -2,5 +2,6 @@ import createDocument from "./createDocument";
 import getAllDocuments from "./getAllDocuments";
 import getDocumentById from "./getDocumentById";
 import updateDocumentById from "./updateDocumentById";
+import deleteDocumentById from "./deleteDocumentById";
 
-export { createDocument, getAllDocuments, getDocumentById, updateDocumentById };
+export { createDocument, getAllDocuments, getDocumentById, updateDocumentById, deleteDocumentById };
