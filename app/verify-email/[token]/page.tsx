f05ea529f89f8988ba37a0e4ce48ed3c6296ebc4
Parames Pasitprachaja <parames.pasitprachaja@gmail.com>
2023-09-11 23:53:20 +0700

    feat(verify-email): connect front to back api, verify email with success and fail message

--- app/verify-email/[token]/page.tsx
+++ app/verify-email/[token]/page.tsx
@@ -1,36 +1,107 @@
 "use client";
 
 import axios from "axios";
-import { useParams } from "next/navigation";
+import Link from "next/link";
 import { useEffect, useState } from "react";
+import { useParams, useRouter } from "next/navigation";
+
+import { Svgs } from "@/assets";
+import { useModal } from "@/hook";
+import { Spinner } from "@/components";
+import { Button } from "@/components/base";
 
 export default function VerifyPage(): JSX.Element {
+  // * router
+  const router = useRouter();
+
   // * get params from url
   const params = useParams();
   const token: string | string[] = params?.token;
 
-  const [isLoading, setIsLoading] = useState(true);
-  const [message, setMessage] = useState("");
+  // * use modal hook
+  const { openModal, CustomModal } = useModal();
+
+  // * spinner state
+  const [isLoading, setIsLoading] = useState(false);
+
+  // * success state
+  const [isSuccess, setIsSuccess] = useState(false);
 
   // * verify email with token
   useEffect(() => {
     const verifyEmail = async () => {
-      const res = await axios.patch(`http://localhost:3030/api/auth/${token}/verify-email`);
-      console.log(res);
-      if (res.status === 200) {
-        setMessage(res.data.message);
-      } else {
-        setMessage(res.data);
+      setIsLoading(true);
+
+      try {
+        const res = await axios.patch(`http://localhost:3030/api/auth/${token}/verify-email`);
+        console.log(res);
+        if (res.status === 200) {
+          setIsSuccess(true);
+        } else {
+        }
+        setIsLoading(false);
+      } catch (error: any) {
+        console.log(error);
+        openModal({
+          title: "Error",
+          message: error.response.data.message,
+          type: "danger"
+        });
+      } finally {
+        setIsLoading(false);
       }
-      setIsLoading(false);
     };
     verifyEmail();
   }, []);
 
   return (
-    <main>
-      <div>Verify Page</div>
-      {isLoading ? <div>Loading...</div> : <div>{message}</div>}
-    </main>
+    <>
+      <main className="w-screen min-h-screen flex flex-col justify-center items-center">
+        {!isLoading && (
+          <>
+            <div className="flex flex-col justify-center items-center gap-8">
+              {isSuccess ? (
+                <>
+                  <div className="text-5xl">Congratulations !</div>
+                  <div className="flex gap-16 justify-center items-center">
+                    <Svgs.Check />
+                    <p className="w-[50%] text-lg">
+                      Your email address has been successfully verified. You can now log in to your
+                      account and start exploring Todo List.
+                    </p>
+                  </div>
+                  <Button.Default className="text-2xl" onClick={() => router.push("/login")}>
+                    Back to Sign In
+                  </Button.Default>
+                </>
+              ) : (
+                <div className="text-3xl flex gap-16 justify-center items-center">
+                  <Svgs.Cross />
+                  <div className="flex flex-col gap-4">
+                    <p>Something went wrong !</p>
+                    <p>please contact our support team. </p>
+                    <p>
+                      email:{" "}
+                      <Link
+                        className="text-blue-600 underline cursor-pointer"
+                        href={"mailto:princem13.test@gmail.com"}
+                      >
+                        princem13.test@gmail.com
+                      </Link>
+                    </p>
+                  </div>
+                </div>
+              )}
+            </div>
+          </>
+        )}
+      </main>
+
+      {/* modal */}
+      <CustomModal />
+
+      {/* spinner */}
+      {isLoading && <Spinner.HourGlass />}
+    </>
   );
 }
