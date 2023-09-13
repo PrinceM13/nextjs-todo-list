ac1d32cb8e0db62e6518ba2ebc886b3e05166252
Parames Pasitprachaja <parames.pasitprachaja@gmail.com>
2023-09-13 14:08:23 +0700

    feat(view): add view to bottom-right of each stckynote on todo-list page and update /api/todo/:id/detail to increase 1 view for each called

--- components/StickyNote/TodoList/index.tsx
+++ components/StickyNote/TodoList/index.tsx
@@ -52,49 +52,62 @@ export default function TodoListStickyNote({
       {todoList.map((todo, idx) => (
         <div
           key={idx}
-          className="flex flex-col gap-4 p-8 rounded-xl bg-yellow-200 hover:bg-yellow-300 active:bg-yellow-400 shadow-xl cursor-pointer"
+          className="flex flex-col justify-between gap-8 p-8 rounded-xl bg-yellow-200 hover:bg-yellow-300 active:bg-yellow-400 shadow-xl cursor-pointer"
           onClick={() => router.push(`/todo-list/${todo?._id}`)}
         >
-          <div
-            onClick={(e) => e.stopPropagation()}
-            className={`flex-1 flex gap-2 flex-grow-0 justify-center items-center rounded-lg py-1 cursor-default ${
-              todo?.isCompleted ? "bg-green-300" : "bg-red-300"
-            }`}
-          >
-            <Input.CheckBox
-              initialValue={todo.isCompleted}
-              onChange={(value) => updateIsCompleted(todo._id ?? "", value)}
-            />
-            <div>{todo?.isCompleted ? "completed" : "incomplete"}</div>
+          <div className="flex flex-col gap-4">
+            <div
+              onClick={(e) => e.stopPropagation()}
+              className={`flex-1 flex gap-2 flex-grow-0 justify-center items-center rounded-lg py-1 cursor-default ${
+                todo?.isCompleted ? "bg-green-300" : "bg-red-300"
+              }`}
+            >
+              <Input.CheckBox
+                initialValue={todo.isCompleted}
+                onChange={(value) => updateIsCompleted(todo._id ?? "", value)}
+              />
+              <div>{todo?.isCompleted ? "completed" : "incomplete"}</div>
+            </div>
+            <div className="font-bold text-xl">{todo.topic}</div>
+            {todo && (
+              <div>
+                <label>member:</label>
+                <div className="flex flex-wrap gap-2 border-2 border-yellow-400 rounded-lg p-2">
+                  {todo?.member?.map((member, idx) => (
+                    <div
+                      key={idx}
+                      className="flex justify-center items-center gap-2 w-fit px-2 py-1 cursor-default text-xs bg-neutral-300 rounded-full shadow-md"
+                    >
+                      <div>{member?.displayName}</div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )}
+            {todo.dueDate && (
+              <div className="flex gap-2">
+                <label>due:</label>
+                <div
+                  className={`${
+                    compare.isOverdue(todo.dueDate) && !todo.isCompleted ? "text-red-600" : ""
+                  }`}
+                >
+                  {converter.dateToString(new Date(todo?.dueDate))}
+                </div>
+              </div>
+            )}
           </div>
-          <div className="font-bold text-xl">{todo.topic}</div>
-          {todo && (
-            <div>
-              <label>member:</label>
-              <div className="flex flex-wrap gap-2 border-2 border-yellow-400 rounded-lg p-2">
-                {todo?.member?.map((member, idx) => (
-                  <div
-                    key={idx}
-                    className="flex justify-center items-center gap-2 w-fit px-2 py-1 cursor-default text-xs bg-neutral-300 rounded-full shadow-md"
-                  >
-                    <div>{member?.displayName}</div>
-                  </div>
-                ))}
-              </div>
-            </div>
-          )}
-          {todo.dueDate && (
-            <div className="flex gap-2">
-              <label>due:</label>
-              <div
-                className={`${
-                  compare.isOverdue(todo.dueDate) && !todo.isCompleted ? "text-red-600" : ""
-                }`}
-              >
-                {converter.dateToString(new Date(todo?.dueDate))}
-              </div>
-            </div>
-          )}
+
+          <div className="text-xs lg:text-sm flex gap-2 text-neutral-400 self-end ">
+            {(todo && todo?.view) || 0 > 0 ? (
+              <>
+                <label>view:</label>
+                <div>{todo.view}</div>
+              </>
+            ) : (
+              <div>no view</div>
+            )}
+          </div>
         </div>
       ))}
     </section>
